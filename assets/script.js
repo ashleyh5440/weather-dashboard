4c14b549bef16b4d22d0cac14f0f8a56a2de3e2b
Ashley <ashleyh5440@gmail.com>
2023-10-27 19:04:11 -0400

    added api and function to get weather data

--- assets/script.js
+++ assets/script.js
@@ -9,17 +9,30 @@ const searchResults = document.getElementById("searchResults");
 const fiveDay = document.getElementById("fiveDay");
 
 //seach button
-searchBtn.addEventListener("click", {
-    let cityName = input.value.trim();
-    if(cityName === "") {
-        alert("Please enter a city");
-    } else {
-        getWeatherData(cityName);
-    }
-});
+searchBtn.addEventListener("click", getWeatherData);
 
-//get api
-function getWeatherData
+//get api\
+const apiKey = "af9558435b5b700340934127adc478ab";
+const city = input.value; //chooses city based on the input
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=af9558435b5b700340934127adc478ab&units=imperial"
+
+function getWeatherData() {
+    fetch(apiUrl)
+    .then((response) => response.json())
+    .then((data) => {
+        if (data.cod === 200) {
+            displayWeatherData(data);
+        } else {
+            alert("City not found. Please check the city name.");
+        }
+    })
+    .catch((error) => {
+        console.error("Error fetching weather data:", error);
+        alert("An error occurred while fetching weather data.");
+    });
+}
 
 //display weather data
-function displayWeatherData 
+function displayWeatherData() {
+
+} 
